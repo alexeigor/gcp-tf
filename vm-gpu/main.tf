2186645fc077abba9f310e5fd316510122ddac9a
Alexey Gorodilov <alexeigor@gmail.com>
2024-01-06 18:12:33 +0300

    update tf

--- vm-gpu/main.tf
+++ vm-gpu/main.tf
@@ -5,8 +5,11 @@ provider "google" {
   credentials = file("/Users/agorodilov/.config/gcloud/application_default_credentials.json")
 
   project = "agor1-409007"
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  zone    = "us-east1-b"
+}
+
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
 }
 
 resource "google_compute_instance" "instance-2" {
@@ -28,28 +31,29 @@ resource "google_compute_instance" "instance-2" {
   enable_display      = false
 
   guest_accelerator {
-    count = 1
-    type  = "projects/agor1-409007/zones/us-central1-a/acceleratorTypes/nvidia-l4"
+    count = 0
+    # type  = "projects/agor1-409007/zones/us-east1-b/acceleratorTypes/nvidia-tesla-a100"
+    type  = "nvidia-tesla-a100"
   }
 
   labels = {
     goog-ec-src = "vm_add-tf"
   }
 
-  machine_type = "g2-standard-4"
+  # machine_type = "g2-standard-4"
+  machine_type = "a2-highgpu-1g"
 
   metadata = {
     ssh-keys = "agorodilov:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCklyTVEZ8GqPikVOTtcsPpooJ4Kr8h2fB5FTzG8s/PUAOZkcHRq90s/FfHJqTbJkk17NWL83XsqdmZavtBBP6cEDHxS8wG7xQch2yLzd95iZcjendPoA1Y2A0aon5KAL+l2nMhYaDlQiYYvJQxE2NQih4S5YznfhfdzyaiPkRe7xnm0HO88Ef0M0LrqCETUf9hyU5VH1ClB5VhDp4gTtSrmR+8/4cJYhbcvGBCXF1NlV13EwLgKTHP6JE5ZyABawqt53GrlO2pF46orMoCAnAf6oVjXdVp8v35FioQk3d+lHfN3M/+YmhCOJTz9gF4W9XjQHZbrYt1dw0XxzPzMrJV agorodilov@Mac-mini-Alexey.local"
   }
 
-  name = "instance-2"
+  name = "instance-gpu"
 
   network_interface {
+    network = "default"
     access_config {
-      network_tier = "PREMIUM"
+      nat_ip = google_compute_address.static.address
     }
-
-    subnetwork = "projects/agor1-409007/regions/us-central1/subnetworks/default"
   }
 
   scheduling {
@@ -57,11 +61,6 @@ resource "google_compute_instance" "instance-2" {
     on_host_maintenance = "TERMINATE"
     preemptible         = true
     provisioning_model  = "SPOT"
-  }
-
-  service_account {
-    email  = "1068491191915-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
   shielded_instance_config {
