96f1062b468f4e900a29f99afc8e6ae378b67fd8
Alexey Gorodilov <alexeigor@gmail.com>
2024-05-19 21:05:28 +0300

    update

--- vm-gpu/main.tf
+++ vm-gpu/main.tf
@@ -15,15 +15,10 @@ resource "google_compute_address" "static" {
 resource "google_compute_instance" "instance-2" {
   boot_disk {
     auto_delete = true
-    device_name = "instance-1"
-
     initialize_params {
-      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20231213a"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
       size  = 150
-      type  = "pd-balanced"
     }
-
-    mode = "READ_WRITE"
   }
 
   can_ip_forward      = false
@@ -44,7 +39,7 @@ resource "google_compute_instance" "instance-2" {
   machine_type = "a2-highgpu-1g"
 
   metadata = {
-    ssh-keys = "agorodilov:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCklyTVEZ8GqPikVOTtcsPpooJ4Kr8h2fB5FTzG8s/PUAOZkcHRq90s/FfHJqTbJkk17NWL83XsqdmZavtBBP6cEDHxS8wG7xQch2yLzd95iZcjendPoA1Y2A0aon5KAL+l2nMhYaDlQiYYvJQxE2NQih4S5YznfhfdzyaiPkRe7xnm0HO88Ef0M0LrqCETUf9hyU5VH1ClB5VhDp4gTtSrmR+8/4cJYhbcvGBCXF1NlV13EwLgKTHP6JE5ZyABawqt53GrlO2pF46orMoCAnAf6oVjXdVp8v35FioQk3d+lHfN3M/+YmhCOJTz9gF4W9XjQHZbrYt1dw0XxzPzMrJV agorodilov@Mac-mini-Alexey.local"
+    ssh-keys = "agorodilov:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCfiiRIq2IGLfGv3GiKo96puAP0dwipW91RxKR5pTJ4FfwR+XMzU6caCG9EOyzD9SyfEgr5+iB76oCLz4OrE3qjRqKOxQJQ5QvGMzdayeLHNB4SKV+rOmVuBAclNMlYM+tBhxsp+VgYFWhZ2qWjbruz1M9H3I6ThUy+3p8jDEJ6bPYDL8LkIkbAUX0WmOBrMhoqgf9pX3UXCqXw/HV3YgFeH/S76o1MBEDHbPRl7fSSjKFw+WrVHf32Rd0vJMSaMnHpQ35YenLb2YSTE81ECXEOt0Q74TcyKhhWoTJCFRJX/cbLJE7ueV1p/hOzxz49Hsrlem2MAFLUmOFnb1dz2vqRGSepKF4AE0+r915HPTeu5ZxfK7scV/TgWpFvG0k66BQ7CczyjApuTO05+SoQA5kwbz9+qBFq/3P+A7PLLCpz6txXC4T1vlOYUJzsVAz1E9KD30gEFvECPiAP2HeVE3uh8NRYmMU9sLrMi3TgWLaFSlFZji+t2dsyna4D3aZhHiqfzPMoumbVnlKE0GJkH9db0NL9BSpltNHX4qV+G9fBqDPoLuvmi4jEXLnd2MmyJ5H1MUQ+DBj0qunCcBWpG9VsEidoulCNpEscMUA5mkoKVMlUdK7oopW3G65uCgc82Jgi0ZfGv+vdI2kj3SIozi2q28glgqchMz/Q2JjOFkmu6w== agorodilov@Alexeys-MacBook-Pro.local"
   }
 
   name = "instance-gpu"
